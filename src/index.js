034c589f016e416b49c67ba4970cac6dbcd14fa8
WerJJJ <marikariwest@gmail.com>
2020-10-14 22:50:07 +0300

    feat: solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let number = "" + n;
+  number = number.split('');
+  if (number[0] == '-'){
+    number.shift();
+  }
+  number.reverse();
+  return +number.join('');
 }
